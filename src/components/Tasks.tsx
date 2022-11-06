52e0b635c248b3e98d90e409f346c5b21c18f3ac
Your Name <you@example.com>
2022-11-06 16:53:40 -0300

    Finalizando style task

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,9 +1,30 @@
 import style from './Tasks.module.scss'
 import Clipboard from '../assets/Clipboard.svg'
-import Unchecked from '../assets/Unchecked.svg'
-import UncheckedHover from '../assets/UncheckedHover.svg'
+
+import { Trash } from 'phosphor-react'
 
 export function Tasks() {
+
+    function handleChangeIconChecked() {
+        const divIcon = document.getElementById(style.iconUnchecked)
+        const p = document.getElementById(style.pUnchecked)
+
+        if(divIcon) {
+            divIcon?.classList.remove(style.iconUnchecked)
+            divIcon?.classList.add(style.iconCheck)
+            divIcon?.setAttribute('id', style.iconCheck)
+            p?.setAttribute('id', style.pChecked)
+        } else {
+            const divIcon = document.getElementById(style.iconCheck)
+            const p = document.getElementById(style.pChecked)
+
+            divIcon?.classList.remove(style.iconCheck)
+            divIcon?.classList.add(style.iconUnchecked)
+            divIcon?.setAttribute('id', style.iconUnchecked)
+            p?.setAttribute('id', style.pUnchecked)
+        }
+    }
+
 	return (
         <div>
     		<header className={style.headerTasks}>
@@ -17,22 +38,17 @@ export function Tasks() {
                	</div>
             </header> 
 
-            <div className={style.contentTask}>
-                <div className={style.withoutTask}>
-                    <img src={Clipboard} alt="Clipboard"/>
-                    <p id={style.pText1}>Você ainda não tem tarefas cadastradas</p>
-                    <p id={style.pText2}>Crie tarefas e organize seus itens a fazer</p>
-                </div>
+            <div className={style.withoutTask}>
+                <img src={Clipboard} alt="Clipboard"/>
+                <p id={style.pText1}>Você ainda não tem tarefas cadastradas</p>
+                <p id={style.pText2}>Crie tarefas e organize seus itens a fazer</p>
+            </div>
 
-                <div className={style.taskList}>
-                    <div>
-                        <div className={style.divCheck}>
-                            <img src={Unchecked} className={style.iconCheck} id="unchecked" />
-                            <img src={UncheckedHover} className={style.iconCheck} id="uncheckedHover" />
-                        </div>
-        
-                        <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                    </div>
+            <div className={style.taskList}>
+                <div>
+                    <div id={style.iconUnchecked} onClick={handleChangeIconChecked} />
+                    <p id={style.pUnchecked}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    <span className={style.trashIcon}><Trash size={20} /></span>
                 </div>
             </div>
         </div>
