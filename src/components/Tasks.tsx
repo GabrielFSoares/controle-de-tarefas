9b224a4c4fd64c3764363665c45e1e4fedcef388
Your Name <you@example.com>
2022-11-06 13:43:41 -0300

    add scss

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,4 +1,7 @@
-import style from './Tasks.module.css'
+import style from './Tasks.module.scss'
+import Clipboard from '../assets/Clipboard.svg'
+import Unchecked from '../assets/Unchecked.svg'
+import UncheckedHover from '../assets/UncheckedHover.svg'
 
 export function Tasks() {
 	return (
@@ -15,8 +18,21 @@ export function Tasks() {
             </header> 
 
             <div className={style.contentTask}>
-                <div>
-                    
+                <div className={style.withoutTask}>
+                    <img src={Clipboard} alt="Clipboard"/>
+                    <p id={style.pText1}>Você ainda não tem tarefas cadastradas</p>
+                    <p id={style.pText2}>Crie tarefas e organize seus itens a fazer</p>
+                </div>
+
+                <div className={style.taskList}>
+                    <div>
+                        <div className={style.divCheck}>
+                            <img src={Unchecked} className={style.iconCheck} id="unchecked" />
+                            <img src={UncheckedHover} className={style.iconCheck} id="uncheckedHover" />
+                        </div>
+        
+                        <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    </div>
                 </div>
             </div>
         </div>
